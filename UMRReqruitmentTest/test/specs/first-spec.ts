86f09b48e0d0b3138d8e81a7094bdd629d21464e
assad <assad@ael-bd.com>
2016-05-28 20:01:33 +0600

    change

--- UMRReqruitmentTest/test/specs/first-spec.ts
+++ UMRReqruitmentTest/test/specs/first-spec.ts
@@ -1,10 +1,13 @@
 ﻿var assert = require('assert');
 
-describe('webdriver.io page', function () {
-    it('should have the right title - the fancy generator way', function () {
-        browser.url('http://www.google.com');
+describe('Google search', function () {
+    it('searches for WebdriverIO', function () {
+        browser.url('http://google.com/');
+        browser.setValue('#lst-ib', 'WebdriverIO');
+        browser.click('#sblsbb .lsb');
+
         var title = browser.getTitle();
+        console.log('Title is: ' + title);
         assert.equal(title, 'Google');
     });
 });
-

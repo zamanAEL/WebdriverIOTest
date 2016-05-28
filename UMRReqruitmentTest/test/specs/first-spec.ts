82dd20589519f371e015db41840239df75ecc79c
assad <assad@ael-bd.com>
2016-05-28 15:30:02 +0600

    change

--- UMRReqruitmentTest/test/specs/first-spec.ts
+++ UMRReqruitmentTest/test/specs/first-spec.ts
@@ -2,7 +2,7 @@
 
 describe('webdriver.io page', function () {
     it('should have the right title - the fancy generator way', function () {
-        browser.url('http://www.yahoo.com');
+        browser.url('http://www.google.com');
         var title = browser.getTitle();
         assert.equal(title, 'Google');
     });

2409bd965fb37417749e5b9c162955e40036386a
assad <assad@ael-bd.com>
2016-05-28 14:34:57 +0600

    initial commit

---
+++ UMRReqruitmentTest/test/specs/first-spec.ts
@@ -0,0 +1,10 @@
+﻿var assert = require('assert');
+
+describe('webdriver.io page', function () {
+    it('should have the right title - the fancy generator way', function () {
+        browser.url('http://www.yahoo.com');
+        var title = browser.getTitle();
+        assert.equal(title, 'Google');
+    });
+});
+
